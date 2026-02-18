a34485dfce5ac03d4b20ce98841be7c3ee7e45c2
amith200127 <amithbr.ramesh@gmail.com>
2026-02-18 23:19:40 +0530

    feat: Add `master.plants` and `master.plant_machine_mapping` tables along with the `etl.fn_log_job_start` function.

--- schema_new/master/Tables/plant_machine_mapping.sql
+++ schema_new/master/Tables/plant_machine_mapping.sql
@@ -1,47 +1,29 @@
 --
--- PostgreSQL database dump
---
-
---
 -- Name: plant_machine_mapping; Type: TABLE; Schema: master; Owner: -
 --
 
 CREATE TABLE master.plant_machine_mapping (
     mapping_id SERIAL NOT NULL,
-    plant_id integer NOT NULL,
-    machine_iot_id integer NOT NULL,
+    company_iot_id integer NOT NULL, -- Refers to master.companies(iot_id)
+    plant_iot_id integer NOT NULL, -- Refers to master.plants(iot_id)
+    machine_iot_id integer NOT NULL, -- Refers to master.machine_info(iot_id)
     created_at timestamp with time zone DEFAULT now()
 );
 
---
--- Name: plant_machine_mapping plant_machine_mapping_pkey; Type: CONSTRAINT; Schema: master; Owner: -
---
-
+-- Primary Key
 ALTER TABLE ONLY master.plant_machine_mapping
     ADD CONSTRAINT plant_machine_mapping_pkey PRIMARY KEY (mapping_id);
 
---
--- Name: plant_machine_mapping uq_plant_machine; Type: CONSTRAINT; Schema: master; Owner: -
---
+-- Unique constraint to prevent duplicate mappings
+ALTER TABLE ONLY master.plant_machine_mapping
+    ADD CONSTRAINT uq_plant_machine UNIQUE (plant_iot_id, machine_iot_id);
+
+-- Foreign Keys
+ALTER TABLE ONLY master.plant_machine_mapping
+    ADD CONSTRAINT plant_machine_mapping_company_iot_id_fkey FOREIGN KEY (company_iot_id) REFERENCES master.companies(iot_id);
 
 ALTER TABLE ONLY master.plant_machine_mapping
-    ADD CONSTRAINT uq_plant_machine UNIQUE (plant_id, machine_iot_id);
-
---
--- Name: plant_machine_mapping plant_machine_mapping_plant_id_fkey; Type: FK CONSTRAINT; Schema: master; Owner: -
---
-
-ALTER TABLE ONLY master.plant_machine_mapping
-    ADD CONSTRAINT plant_machine_mapping_plant_id_fkey FOREIGN KEY (plant_id) REFERENCES master.plants(plant_id);
-
---
--- Name: plant_machine_mapping plant_machine_mapping_machine_iot_id_fkey; Type: FK CONSTRAINT; Schema: master; Owner: -
---
+    ADD CONSTRAINT plant_machine_mapping_plant_iot_id_fkey FOREIGN KEY (plant_iot_id) REFERENCES master.plants(iot_id);
 
 ALTER TABLE ONLY master.plant_machine_mapping
     ADD CONSTRAINT plant_machine_mapping_machine_iot_id_fkey FOREIGN KEY (machine_iot_id) REFERENCES master.machine_info(iot_id);
-
---
--- PostgreSQL database dump complete
---
-
--- schema_new/master/Tables/plants.sql
+++ schema_new/master/Tables/plants.sql
@@ -1,35 +1,31 @@
 --
--- PostgreSQL database dump
---
-
---
 -- Name: plants; Type: TABLE; Schema: master; Owner: -
 --
 
 CREATE TABLE master.plants (
-    plant_id SERIAL NOT NULL,
+    iot_id SERIAL NOT NULL,
     company_iot_id integer NOT NULL,
-    plant_name text,
-    plant_city text,
-    created_at timestamp with time zone DEFAULT now()
+    plant_id text NOT NULL, -- Business unique ID
+    plant_code text,
+    description text,
+    is_primary boolean DEFAULT false,
+    effective_from_date timestamp with time zone DEFAULT now(),
+    effective_to_date timestamp with time zone DEFAULT '9999-12-31 23:59:59+00',
+    updated_by text,
+    updated_ts timestamp with time zone DEFAULT now()
 );
 
---
--- Name: plants plants_pkey; Type: CONSTRAINT; Schema: master; Owner: -
---
-
+-- Primary Key
 ALTER TABLE ONLY master.plants
-    ADD CONSTRAINT plants_pkey PRIMARY KEY (plant_id);
+    ADD CONSTRAINT plants_pkey PRIMARY KEY (iot_id);
 
---
--- Name: plants plants_company_id_fkey; Type: FK CONSTRAINT; Schema: master; Owner: -
---
-
+-- Foreign Key
 ALTER TABLE ONLY master.plants
     ADD CONSTRAINT plants_company_iot_id_fkey FOREIGN KEY (company_iot_id) REFERENCES master.companies(iot_id);
 
---
--- PostgreSQL database dump complete
---
+-- Unique constraint for business logic
+ALTER TABLE ONLY master.plants
+    ADD CONSTRAINT plants_company_plant_unique UNIQUE (company_iot_id, plant_id);
 
-
+-- Indices for performance
+CREATE INDEX plants_lookup_idx ON master.plants (company_iot_id, plant_code) WHERE (effective_to_date > now());
